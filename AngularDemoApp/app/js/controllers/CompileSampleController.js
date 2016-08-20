ca2fc51dd24f673d10be7510c1d28a5a825585db
Quiteblank <iamquiteblank@gmail.com>
2016-08-20 10:38:33 -0700

    FInished the $compile service tutorial, but the page doesn't work because it's insecure.

---
+++ AngularDemoApp/app/js/controllers/CompileSampleController.js
@@ -0,0 +1,12 @@
+﻿'use strict'
+
+// Use the cache factory to store key/value pairs
+eventsApp.controller('CompileSampleController',
+    function CompileSampleController($scope, $compile) {
+
+        $scope.appendDivToElement = function (markup) {
+            return $compile(markup)($scope).appendTo(angular.element("#appendHere"));
+        }
+
+    }
+);
